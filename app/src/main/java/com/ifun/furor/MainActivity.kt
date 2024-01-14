68667bb768898fefde843ff1d0c476e79ceb0650
AlvaroSan98 <38313342+AlvaroSan98@users.noreply.github.com>
2024-01-14 15:53:00 +0100

    Initial commit

---
+++ app/src/main/java/com/ifun/furor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ifun.furor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
